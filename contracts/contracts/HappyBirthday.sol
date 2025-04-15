3594b1e1586bc423f051d0e852b1c4982974aa8e
motemotech <i.am.nicoshark@gmail.com>
2025-04-16 04:45:59 +0900

    delete unused imports

--- contracts/contracts/HappyBirthday.sol
+++ contracts/contracts/HappyBirthday.sol
@@ -4,8 +4,6 @@ pragma solidity 0.8.28;
 import {SelfVerificationRoot} from "@selfxyz/contracts/contracts/abstract/SelfVerificationRoot.sol";
 import {ISelfVerificationRoot} from "@selfxyz/contracts/contracts/interfaces/ISelfVerificationRoot.sol";
 import {SelfCircuitLibrary} from "@selfxyz/contracts/contracts/libraries/SelfCircuitLibrary.sol";
-import {Formatter} from "@selfxyz/contracts/contracts/libraries/Formatter.sol";
-import {CircuitAttributeHandler} from "@selfxyz/contracts/contracts/libraries/CircuitAttributeHandler.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
